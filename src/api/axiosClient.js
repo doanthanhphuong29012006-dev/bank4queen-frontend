4efdb56b353105db204210c91dcb86c192818f36
phuongne <doanthanhphuong29012006@gmail.com>
2026-08-04 00:37:19 +0700

    Update API baseURL for production

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 'bank4queen-backend-production.up.railway.app', 
   headers: {
     'Content-Type': 'application/json',
   },
